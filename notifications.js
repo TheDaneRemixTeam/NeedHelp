47a16b19ea53e0fb428841d45935ace0f2488c1b
chelsealuttrall <chelsealuttrall@gmail.com>
2020-03-22 22:32:18 -0600

    Adding api files and info for back end functions

---
+++ notifications.js
@@ -0,0 +1,9 @@
+//link to dashboard: https://app.onesignal.com
+//login: 
+// organization: The Dane Remix Team
+//email: chelsealuttrall@gmail.com
+//pw: the1&only
+
+//rest api key: N2IxNWY0ZTQtODRjMS00ODMxLWE4YjgtY2YyZDkxZGQ3MWM5
+//auth key: OTVlN2U1MGItODQ3MS00YWU5LWJmZTktZjZkYzhjMjg0Y2E2
+//app id: a8dc9719-288c-4e81-adf4-e69fcefe3976
