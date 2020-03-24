7729b42c3723778c18021a141b6689fe44a720cc
chelsealuttrall <chelsealuttrall@gmail.com>
2020-03-23 20:48:49 -0600

    script ref for notifications

--- notifications.js
+++ notifications.js
@@ -7,3 +7,13 @@
 //rest api key: N2IxNWY0ZTQtODRjMS00ODMxLWE4YjgtY2YyZDkxZGQ3MWM5
 //auth key: OTVlN2U1MGItODQ3MS00YWU5LWJmZTktZjZkYzhjMjg0Y2E2
 //app id: a8dc9719-288c-4e81-adf4-e69fcefe3976
+
+//at the top of html page head 
+
+
+// Other init Options
+welcomeNotification: {
+    "title": "My Custom Title",
+    "message": "Thanks for subscribing!",
+    // "url": "" /* Leave commented for the notification to not open a window on Chrome and Firefox (on Safari, it opens to your webpage) */
+}
