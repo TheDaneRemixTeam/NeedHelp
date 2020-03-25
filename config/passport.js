f61c099e25e707a660d76034f8d8d4428779e7df
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-25 10:09:06 -0600

    Create passport.js
    This will handle what happens when a user tries to log in.

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable indent */
+// dependencies
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+
+// Import the models folder
+var db = require("../models");
+
+// Telling passport that we want to login using email
+passport.use(new LocalStrategy(
+   {
+       usernameField: "email"
+   },
+   function(email, password, done) {
+    // When the user tries to sign in
+    db.User.findOne({
+        where: {
+            email: email
+        }
+    }).then(function(dbUser) {
+        // If there is no user with the email
+        if (!dbUser) {
+            return done(null, false, {
+                message: "Incorrect email."
+            });
+        } 
+        // If the user is registered, but the password is inncorrect
+        else if (!dbUser.validPassword(password)) {
+            return done(null, false, {
+                message: "Inncorrect password."
+            });
+        }
+        // if none of the above return the user
+        return done(null, dbUser);
+    });
+   } 
+));
