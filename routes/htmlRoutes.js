b88f12e3e2f90c7f2abcb733079dd2919939e3d2
Jordan Servos <jservos11@yahoo.com>
2020-03-25 12:30:32 -0600

    Added html routes for signup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,28 +1,55 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    // Load index page
+    app.get("/", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.render("index", {
+                msg: "Welcome!",
+                examples: dbExamples
+            });
+        });
     });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    // eslint-disable-next-line prettier/prettier
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+    app.get("/create", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.render("gigcreate", {
+                // msg: "Welcome!",
+                // examples: dbExamples
+            });
+        });
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+    app.get("/helpersignup", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.render("helpersignup", {
+                // msg: "Welcome!",
+                // examples: dbExamples
+            });
+        });
+    });
+
+    app.get("/reqsignup", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.render("requestersignup", {
+                // msg: "Welcome!",
+                // examples: dbExamples
+            });
+        });
+    });
+
+    // Load example page and pass in an example by id
+    app.get("/example/:id", function(req, res) {
+        // eslint-disable-next-line prettier/prettier
+        db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+            res.render("example", {
+                example: dbExample
+            });
+        });
+    });
+
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+        res.render("404");
+    });
 };
