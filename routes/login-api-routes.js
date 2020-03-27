5b35a25cbab07d85ada6e75f34b3885d408bdff1
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-26 20:17:56 -0600

    Routes created

--- routes/login-api-routes.js
+++ routes/login-api-routes.js
@@ -6,7 +6,7 @@ var db = require("../models");
 var passport = require("../config/passport.js");
 
 module.exports = function(app) {
-    // Route for signing up a new user
+    // Route for signing up a new user 
     app.post("/api/signup", function(req, res) {
         console.log(req.body);
         db.User.create(req.body).then(function(result) {
