3b2b683931b7abfe0987911adcad825b4b338962
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-28 11:29:57 -0600

    Update login-api-routes.js
    Created the routes for logging into the website and getting user data

--- routes/login-api-routes.js
+++ routes/login-api-routes.js
@@ -6,11 +6,29 @@ var db = require("../models");
 var passport = require("../config/passport.js");
 
 module.exports = function(app) {
+    // route for logging in
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json("/");
+    });
+
     // Route for signing up a new user 
     app.post("/api/signup", function(req, res) {
         console.log(req.body);
-        db.User.create(req.body).then(function(result) {
-            res.json(result);
+        db.User.create(req.body).then(function() {
+            res.redirect(307, "/api/login");
+        }).catch(function(err) {
+            console.log(err);
+            res.json(err);
         }); 
+    });
+
+    // route for getting user data
+    app.get("/api/user_data", function(req, res) {
+        if (!req.user) {
+            res.json({});
+        }
+        else {
+            res.json(req.user);
+        }
     });
 };
