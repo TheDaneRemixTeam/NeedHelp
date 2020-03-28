e8e52e993622ff4a1c1e73308be8e6bb3f594c32
Jordan Servos <jservos11@yahoo.com>
2020-03-28 14:26:55 -0600

    added claim functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,7 +18,9 @@ module.exports = function(app) {
     // Delete an example by id
     app.delete("/api/examples/:id", function(req, res) {
         // eslint-disable-next-line prettier/prettier
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function(
+            dbExample
+        ) {
             res.json(dbExample);
         });
     });
@@ -34,19 +36,38 @@ module.exports = function(app) {
         });
     });
 
-
-
     app.post("/api/create", function(req, res) {
         if (req.user) {
-
+            req.body.poster = req.user.firstname;
+            req.body.UserId = req.user.id;
         } else {
-            req.body.poster = "testuser"
+            req.body.poster = "testuser";
         }
         db.Post.create(req.body).then(function(dbPost) {
             res.json(dbPost);
         });
     });
 
+    app.post("/api/gigview/:id", function(req, res) {
+        if (req.user) {
+            req.body.claimed = 1;
+            req.body.helperID = req.user.id;
+        }
+        db.Post.update(req.body).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
 
-
+    app.put("/api/gigview/:id", function(req, res) {
+        console.log("Claim", req.params, req.user)
+        db.Post.update({
+            helperID: req.user.id,
+            claimed: 1
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function(result) {res.json(result);});
+        
+    });
 };
