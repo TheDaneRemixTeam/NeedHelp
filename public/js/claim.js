39469d98cbc83ce1af4854b58380ee077cdf350a
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-31 11:24:40 -0600

    Putting document.ready in claim.js
    I noticed that when a user is NOT logged in the button will take them to the account page. I am going to put in a code that will check to see if someone is logged in. If they are not the Accept! button will redirect them to login.

--- public/js/claim.js
+++ public/js/claim.js
@@ -1,13 +1,16 @@
-$("#approve").on("click", function(event) {
-  event.preventDefault();
-  console.log("This button works!");
-  let gigId = $(this).attr("data-id");
-  $.ajax({
-    url: "/api/gigview/" + gigId,
-    method: "PUT"
-  }).then(function(data) {
-    console.log(data);
+$(document).ready(function () {
 
-    // window.location.href = "/account";
+  $("#approve").on("click", function (event) {
+    event.preventDefault();
+    console.log("This button works!");
+    let gigId = $(this).attr("data-id");
+    $.ajax({
+      url: "/api/gigview/" + gigId,
+      method: "PUT"
+    }).then(function (data) {
+      console.log(data);
+
+      // window.location.href = "/account";
+    });
   });
-});
+})
