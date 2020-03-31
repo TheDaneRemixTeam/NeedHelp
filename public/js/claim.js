3e68dfc1b33c30d4dc2ad1982bfa0b5d403b0a55
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-31 11:47:28 -0600

    Rerouting a success!
    I was able to write into the script that would check to see if the user was logged in and act accordingly.

--- public/js/claim.js
+++ public/js/claim.js
@@ -1,16 +1,28 @@
+var loggedOutLink = "window.location.href='/login'";
+var loggedInLink = "window.location.href='/account'";
+
 $(document).ready(function () {
+  $.get("/api/user_data").then(function (data) {
+    if (!data.firstname) {
+      console.log("No one is logged in.");
+      $("#approve").attr("onclick", loggedOutLink);
+    } else {
+      console.log(`${data.firstname} is logged in.`);
+      $("#approve").attr("onclick", loggedInLink);
+      $("#approve").on("click", function (event) {
+        event.preventDefault();
+        console.log("This button works!");
+        let gigId = $(this).attr("data-id");
+        $.ajax({
+          url: "/api/gigview/" + gigId,
+          method: "PUT"
+        }).then(function (data) {
+          console.log(data);
 
-  $("#approve").on("click", function (event) {
-    event.preventDefault();
-    console.log("This button works!");
-    let gigId = $(this).attr("data-id");
-    $.ajax({
-      url: "/api/gigview/" + gigId,
-      method: "PUT"
-    }).then(function (data) {
-      console.log(data);
-
-      // window.location.href = "/account";
-    });
+          // window.location.href = "/account";
+        });
+      });
+    }
   });
-})
+});
+
